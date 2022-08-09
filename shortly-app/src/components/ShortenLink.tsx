02e1f14d0c626abf3cddc1d08a62523ce3412652
vijaykumarvemana <vijaykumarvemana18@gmail.com>
2022-08-10 00:13:09 +0200

    button bg fixed

--- shortly-app/src/components/ShortenLink.tsx
+++ shortly-app/src/components/ShortenLink.tsx
@@ -5,7 +5,7 @@ function ShortenLink() {
     <div className=' bg-white d-flex justify-content-between iteams-center text-dark py-3 '>
         <p className=' my-2 mx-3 fw-blod'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, rerum.</p>
         <div className='d-flex justify-content-between'>
-            <p className='text-info my-2 mx-3 fw-blod' >Lorem ipsum dolor sit amet.</p>
+            <p className='short-link my-2 mx-3 fw-blod' >Lorem ipsum dolor sit amet.</p>
             <button className=' btn btn-info text-light  px-3 mx-3'>Copy</button>
         </div>
     </div>
